cacec031011896c0a451a67efc3f298913884e7f
gokul3710 <gokulkrishnawaryar@gmail.com>
2022-12-28 12:47:34 +0530

    Day 1

---
+++ lesson1/build/js/script.js
@@ -0,0 +1,13 @@
+"use strict";
+let lang = `Gokul`;
+let username = `Hello <br> I am ${lang}`;
+console.log(username);
+let a = 2;
+console.log(a);
+let b = "6";
+let intb = +b;
+console.log(a * intb);
+let title = document.createElement('div');
+title.innerHTML = username;
+title.classList.add("title");
+document.body.appendChild(title);
---
+++ lesson1/src/script.ts
@@ -0,0 +1,18 @@
+let lang = `Gokul`
+let username = `Hello <br> I am ${lang}`
+console.log(username);
+let a: number = 2
+console.log(a);
+let b: string = "6"
+let intb: number =  +b;
+console.log(a *intb);
+
+let title = document.createElement('div');
+title.innerHTML = username
+title.classList.add("title")
+
+document.body.appendChild(title)
+
+
+
+
